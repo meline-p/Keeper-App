3a858a06aa1f75abca8492c313c304a4c7708957
{Meline} <{meline.pischedda@gmail.com}>
2022-11-17 01:32:05 +0100

    Set up the app

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>I am a title</h1>
+            <p>I am a content</p>
+        </div>
+    )
+}
+
+export default Note;
